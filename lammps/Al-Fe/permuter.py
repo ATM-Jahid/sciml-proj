b0f385d9dc9e078b518f6c901c002bf24fbd3329
ATM Jahid Hasan <atmjahidhasan@gmail.com>
2021-12-03 14:20:42 -0500

    Add Al-Fe simulations

--- lammps/Al-Fe/permuter.py
+++ lammps/Al-Fe/permuter.py
@@ -9,6 +9,6 @@ perm = [[x, y] for x in comps for y in tilts]
 
 for itr in perm:
     os.system(f'$HOME/builds/UMoly/scripts/replicator.py -i grain \
-            -x 50 -y 200 -z 15 -a {itr[1]} -M {itr[0]} \
-            -o umo_c{itr[0]}_a{itr[1]}_')
-    os.system(f'$HOME/builds/lammps/build/lmp -i in.umo_c{itr[0]}_a{itr[1]}_1')
+            -l 4 -x 40 -y 150 -z 15 -a {itr[1]} -M {itr[0]} \
+            -o alfe_c{itr[0]}_a{itr[1]}_')
+    os.system(f'$HOME/builds/lammps/build/lmp -i in.alfe_c{itr[0]}_a{itr[1]}_1')
